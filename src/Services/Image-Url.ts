46a74ad0d026829de0407b46fd7a085a4a550ab8
Omar-Obide <3omar.obide@gmail.com>
2024-09-09 02:10:20 +0300

    Handling Games with no Image

--- src/Services/Image-Url.ts
+++ src/Services/Image-Url.ts
@@ -1,6 +1,7 @@
+import ImagePlacholder from '../assets/no-image-placeholder-6f3882e0.webp'
 
 const GetCroppedImage = (url: string) => {
-    if (!url) return ''
+    if (!url) return ImagePlacholder
     //https://media.rawg.io/media/crop/600/400/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg
     const index = url.indexOf("io/media") + "io/media".length
     const newUrl = url.slice(0, index) + "/crop/600/400" + url.slice(index)
